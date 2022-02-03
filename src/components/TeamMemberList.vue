f1a916cbe463db471c50f8a83e5e84e37871a0f6
Ashik Nesin <48757775+cb-ashik@users.noreply.github.com>
2022-02-03 15:12:43 +0530

    Initialize dev

---
+++ src/components/TeamMemberList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <b-card >
+      <ul class="list-unstyled" v-for="member in members" :key="member.id">
+        <b-media tag="li" :class="member.id < members.length -1 && 'border-bottom  border-secondary'">
+          <template #aside>
+            <b-avatar
+              width="64"
+              alt="placeholder"
+              src="https://picsum.photos/125/125/?image=58"
+            ></b-avatar>
+
+            <div class="member-info">
+
+          <h5 class="mt-0 mb-1">{{member.name }}</h5>
+          <p class="mb-0">
+            {{ member.email }}
+          </p>
+          <p class="mb-0">
+            {{ member.role }}
+          </p>
+          </div>
+
+          </template>
+
+        </b-media>
+      </ul>
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  name: 'TeamMemberList',
+  props: {
+    members: {
+      type: Array,
+      default: () => [],
+    },
+  },
+})
+export default class TeamMemberList extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+.member-info{
+margin: 12px;
+text-align: left;
+margin-top: 4px;
+
+}
+</style>
---
+++ src/store/index.ts
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
+import { TeamMember, MemberRole } from "@/types";
+
+const vuexPersist = new VuexPersist({
+  key: "workshop",
+  storage: window.localStorage,
+});
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    members: Array.from({ length: 5 }).map((_, id) => ({
+      id,
+      name: `John Doe ${id}`,
+      email: `john.doe@chargebee.com ${id}`,
+    })) as TeamMember[],
+  },
+  mutations: {
+    addMember(state, member: any) {
+      state.members.push(member);
+    },
+  },
+  getters: {
+    listMembers(state) {
+      return state.members;
+    },
+  },
+  actions: {},
+  modules: {},
+  plugins: [vuexPersist.plugin],
+});
